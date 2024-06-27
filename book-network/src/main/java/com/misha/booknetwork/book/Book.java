30f28fa58e5c3cc21da613a6d9a6afc5a2a3c130
Mishar025 <mishamay583@gmail.com>
2024-06-26 20:31:24 -0400

    Book entity

---
+++ book-network/src/main/java/com/misha/booknetwork/book/Book.java
@@ -0,0 +1,42 @@
+package com.misha.booknetwork.book;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+@Entity
+
+public class Book {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String title;
+
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+
+    private String bookCover;
+    private boolean archived;
+
+    private boolean shareable;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime lastModifiedDate;
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+
+
+
+
+
+}
