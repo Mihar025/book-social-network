19476ad59f321ad4d6f9a92027be84b5cedf6feb
Mishar025 <mishamay583@gmail.com>
2024-06-29 14:29:08 -0400

    added Book saving method

---
+++ book-network/src/main/java/com/misha/booknetwork/FeedBackControler/FeedbackController.java
@@ -0,0 +1,36 @@
+package com.misha.booknetwork.FeedBackControler;
+
+import com.misha.booknetwork.FeedBackRequests.FeedBackRequest;
+import com.misha.booknetwork.FeedBackService.FeedBackService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("feedbacks")
+@RequiredArgsConstructor
+@Tag(name = "Feedback")
+public class FeedbackController {
+
+    private final FeedBackService service;
+
+    @PostMapping
+    public ResponseEntity<Integer> saveFeedBack(
+            @Valid @RequestBody FeedBackRequest request,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.save(request, connectedUser));
+
+
+    }
+
+
+
+}
