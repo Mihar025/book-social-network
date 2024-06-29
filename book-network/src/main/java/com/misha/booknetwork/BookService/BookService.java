244ebb4309b4555fd27c43e66eb66d2924a3d89f
Mishar025 <mishamay583@gmail.com>
2024-06-29 12:40:57 -0400

    added Book saving method

--- book-network/src/main/java/com/misha/booknetwork/BookService/BookService.java
+++ book-network/src/main/java/com/misha/booknetwork/BookService/BookService.java
@@ -179,7 +179,9 @@ private final BookMapper bookMapper;
         if(!Objects.equals(book.getOwner().getId(), user.getId())){
             throw new OperationNotPermittedException("You cannot borrow or return your own book");
         }
-        BookTransactionHistory bookTransactionHistory = bookTransactionHistoryRepository.findByBookIdAndUserId(bookId, user.getId());
-        return null;
+        BookTransactionHistory bookTransactionHistory = bookTransactionHistoryRepository.findByBookIdAndUserId(bookId, user.getId())
+                .orElseThrow(() -> new OperationNotPermittedException("You didnt borrow this book"));
+        bookTransactionHistory.setReturned(true);
+        return bookTransactionHistoryRepository.save(bookTransactionHistory).getId();
     }
 }
