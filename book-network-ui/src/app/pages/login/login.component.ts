a01dd71b1858d7997260da51ca19137f3eab641f
Mishar025 <mishamay583@gmail.com>
2024-06-30 19:03:09 -0400

    added Login page Angular

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import {AuthenticationRequest} from "../../services/models/authentication-request";
+import {FormsModule} from "@angular/forms";
+import {Router} from '@angular/router';
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {CommonModule} from "@angular/common";
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -13,4 +21,29 @@ export class LoginComponent {
     authRequest: AuthenticationRequest = {email: '', password: ''};
     errorMsg: Array<string> = [];
 
+    constructor(
+      private router: Router,
+      private authService: AuthenticationService,
+    //another service
+    ) {
+    }
+
+  login() {
+  this.errorMsg = [];
+  this.authService.authenticate({
+    body: this.authRequest
+  }).subscribe({
+    next:() => {
+      this.router.navigate(['books'])
+    },
+    error: (err) => {
+      console.log(err);
+    }
+  })
+  }
+
+
+  register() {
+      this.router.navigate(['register'])
+  }
 }
--- book-network/src/main/java/com/misha/booknetwork/config/BeansConfig.java
+++ book-network/src/main/java/com/misha/booknetwork/config/BeansConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,6 +13,14 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.springframework.http.HttpHeaders.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -38,6 +47,29 @@ public class BeansConfig {
     @Bean
     public AuditorAware<Integer> auditorAware(){
         return new ApplicationAuditAware();
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.ORIGIN,
+                CONTENT_TYPE,
+                ACCEPT,
+                AUTHORIZATION
+        ));
+        configuration.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST",
+                "DELETE",
+                "PUT",
+                "PATCH"
+        ));
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
     }
 }
 
