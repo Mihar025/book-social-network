6ff2c42b2408ec8ff1e25b9a0b8b49786264c191
Mishar025 <mishamay583@gmail.com>
2024-06-30 16:13:07 -0400

    added Book saving method

---
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {AuthenticationRequest} from "../../services/models/authentication-request";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+    authRequest: AuthenticationRequest = {email: '', password: ''};
+    errorMsg: Array<string> = [];
+
+}
