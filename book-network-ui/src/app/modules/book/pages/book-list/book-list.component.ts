10dbd95b9714c28c47072e3ee8fbe8cdce536fca
Mishar025 <mishamay583@gmail.com>
2024-07-02 19:29:14 -0400

    added Login page Angular

--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -1,15 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {BookService} from "../../../../services/services/book.service";
 import {Router} from "@angular/router";
+import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
 export class BookListComponent implements OnInit{
+  bookResponse: PageResponseBookResponse = {};
+   page = 0;
+   size = 5;
 
     constructor(
       private bookService: BookService,
@@ -17,5 +24,18 @@ export class BookListComponent implements OnInit{
     ) {
     }
 
+    ngOnInit() {
+     this.findAllBooks();
+    }
 
+  private findAllBooks() {
+    this.bookService.findAllBooks({
+      page: this.page,
+      size: this.size
+      }).subscribe({
+      next: (books => {
+          this.bookResponse = books;
+      })
+      });
+  }
 }
