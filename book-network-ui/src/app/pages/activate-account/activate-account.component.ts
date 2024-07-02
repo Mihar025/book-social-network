00e45a7b8b28176f5878ffc3522979895b78da30
Mishar025 <mishamay583@gmail.com>
2024-07-01 20:41:33 -0400

    added Login page Angular

--- book-network-ui/src/app/pages/activate-account/activate-account.component.ts
+++ book-network-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {CodeInputModule} from "angular-code-input";
 
 @Component({
   selector: 'app-activate-account',
   standalone: true,
-  imports: [],
+  imports: [
+    CodeInputModule
+  ],
   templateUrl: './activate-account.component.html',
   styleUrl: './activate-account.component.scss'
 })
 export class ActivateAccountComponent {
 
+  message = '';
+  isOkay = true;
+  submitted = false;
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+
+  onCodeCompleted($event: string) {
+
+  }
 }
