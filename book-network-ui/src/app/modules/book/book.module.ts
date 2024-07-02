0cc8c42dd18a9713406c2616dadb152a1061a475
Mishar025 <mishamay583@gmail.com>
2024-07-02 19:14:59 -0400

    added Login page Angular

--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
+import {MainComponent} from "./pages/main/main.component";
+import {BookListComponent} from "./pages/book-list/book-list.component";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    BookRoutingModule
+    BookRoutingModule,
   ]
 })
 export class BookModule { }
