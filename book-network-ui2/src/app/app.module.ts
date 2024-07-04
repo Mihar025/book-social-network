07836480a452dd9d6aaa0ff50ff4974a98c724a8
Mishar025 <mishamay583@gmail.com>
2024-07-04 12:23:29 -0400

    added Login page Angular

---
+++ book-network-ui2/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import {AppComponent} from "./app.component";
+import {HttpTokenInterceptor} from "./services/interceptor/http-token.interceptor";
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "./app-routing.module";
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true
+    },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
